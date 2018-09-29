5f7adaa48c2e45b9c75b795a9d47a8224870db09
apivovarchik <sf_beton81@mail.ru>
2018-09-30 01:37:36 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsPair = {};
+  let bracketsType = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let openingBracket = bracketsConfig[i][0];
+    let closingBracket = bracketsConfig[i][1];
+    bracketsPair[openingBracket] = closingBracket;
+    bracketsPair[closingBracket] = openingBracket;
+    bracketsType[openingBracket] = "opening";
+    bracketsType[closingBracket] = "closing";
+  }
+  for (let k = 0; k < str.length; k++) {
+    let currBracket = str.charAt(k);
+    let topBracket = stack[stack.length - 1];
+    if (bracketsType[currBracket] == "opening") {
+      stack.push(currBracket);
+    } else if (bracketsType[currBracket] == "closing" && currBracket != bracketsPair[currBracket] && stack.length == 0) {
+      return false;
+    } else if (bracketsType[currBracket] == "closing" && topBracket != bracketsPair[currBracket] && currBracket != bracketsPair[currBracket]) {
+      return false;
+    } else if (bracketsType[currBracket] == "closing" && topBracket == bracketsPair[currBracket]) {
+      stack.pop();
+    } else if (bracketsType[currBracket] == "closing" && stack.length == 0 && currBracket == bracketsPair[currBracket]) {
+      stack.push(currBracket);
+    } else if (bracketsType[currBracket] == "closing" && stack.length > 0 && topBracket == bracketsPair[currBracket]) {
+      stack.pop();
+    } else if (bracketsType[currBracket] == "closing" && stack.length > 0 && currBracket == bracketsPair[currBracket]) {
+      stack.push(currBracket);
+    }
+  }
+  return (stack.length == 0);
 }
